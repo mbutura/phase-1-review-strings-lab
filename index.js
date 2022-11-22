80dd357c28a6f688302e055704b4b87881504f4a
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-11-22 15:25:07 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+
+let currentUser =  'alois';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting  = `Welcome, ${currentUser[0]}!`
